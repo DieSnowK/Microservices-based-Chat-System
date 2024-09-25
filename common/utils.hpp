1e7dd6ee8a93ba772373bfe0076ee65cc923e8f0
DieSnowK <1752351098@qq.com>
2024-09-25 14:25:00 +0000

    utils.hpp done

---
+++ common/utils.hpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <atomic>
+#include <random>
+#include <iomanip>
+#include "logger.hpp"
+
+namespace SnowK
+{
+    // TODO 研究随机化
+    // 生成一个由16位随机字符组成的字符串作为唯一ID
+    std::string UUID()
+    {
+        // 1.生成6个0~255之间的随机数字 -- 生成12位16进制字符
+        std::random_device rd;                                   // 实例化设备随机数对象, 用于生成设备随机数
+        std::mt19937 generator(rd());                            // 以设备随机数为种子, 实例化伪随机数对象
+        std::uniform_int_distribution<int> distribution(0, 255); // 限定数据范围
+
+        std::stringstream ss;
+        for (int i = 0; i < 6; i++)
+        {
+            ss << std::setw(2) << std::setfill('0') << std::hex << distribution(generator);
+        }
+
+        // 2.通过一个静态变量生成一个2字节的编号数字 -> 生成4位16进制数字字符
+        static std::atomic<short> idx(0);
+        short tmp = idx.fetch_add(1);
+        ss << std::setw(4) << std::setfill('0') << std::hex << tmp;
+
+        return ss.str();
+    }
+
+    // std::string VCode()
+    // {
+    //     std::random_device rd;                                 // 实例化设备随机数对象-用于生成设备随机数
+    //     std::mt19937 generator(rd());                          // 以设备随机数为种子，实例化伪随机数对象
+    //     std::uniform_int_distribution<int> distribution(0, 9); // 限定数据范围
+
+    //     std::stringstream ss;
+    //     for (int i = 0; i < 4; i++)
+    //     {
+    //         ss << distribution(generator);
+    //     }
+    //     return ss.str();
+    // }
+
+    // TODO 研究流输入输出
+    bool ReadFile(const std::string &filename, std::string &body)
+    {
+        std::ifstream ifs(filename, std::ios::binary | std::ios::in);
+        if (ifs.is_open() == false)
+        {
+            LOG_ERROR("Failed to open file {}", filename);
+            return false;
+        }
+
+        ifs.seekg(0, std::ios::end); // 跳转到文件末尾
+        size_t flen = ifs.tellg();   // 获取当前偏移量 -> 文件大小
+        ifs.seekg(0, std::ios::beg); // 跳转到文件起始
+
+        body.resize(flen);
+        ifs.read(&body[0], flen);
+        if (ifs.good() == false)
+        {
+            LOG_ERROR("Failed to read file", filename);
+            ifs.close();
+            return false;
+        }
+
+        ifs.close();
+        return true;
+    }
+
+    bool WriteFile(const std::string &filename, const std::string &body)
+    {
+        std::ofstream ofs(filename, std::ios::out | std::ios::binary | std::ios::trunc);
+        if (ofs.is_open() == false)
+        {
+            LOG_ERROR("Failed to open file {}", filename);
+            return false;
+        }
+
+        ofs.write(body.c_str(), body.size());
+        if (ofs.good() == false)
+        {
+            LOG_ERROR("Failed to write file", filename);
+            ofs.close();
+            return false;
+        }
+
+        ofs.close();
+        return true;
+    }
+}
---
+++ file/source/file_server.cc
@@ -0,0 +1,31 @@
+#include "speech_server.hpp"
+
+DEFINE_bool(mode, false, "true: Release, false: Debug");
+DEFINE_string(log_file, "", "In Release, specify the output file of the log");
+DEFINE_int32(log_level, 0, "In Release, specify the log output level");
+
+DEFINE_string(registry_host, "http://127.0.0.1:2379", "Service registry address");
+DEFINE_string(base_service, "/service", "The root directory of service monitoring");
+DEFINE_string(instance_name, "/file_service/instance", "Name of the current instance");
+DEFINE_string(access_host, "127.0.0.1:10002", "The external access address of the current instance");
+
+// DEFINE_string(storage_path, "./data/", "当前实例的外部访问地址");
+
+DEFINE_int32(listen_port, 10002, "The RPC server listens on the port");
+DEFINE_int32(rpc_timeout, -1, "RPC call timeout period");
+DEFINE_int32(rpc_threads, 1, "The number of I/O threads of the rpc");
+
+int main(int argc, char *argv[])
+{
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    SnowK::InitLogger(FLAGS_mode, FLAGS_log_file, FLAGS_log_level);
+
+    SnowK::FileServerBuilder fsb;
+    ssb.Make_RPC_Server(FLAGS_listen_port, FLAGS_rpc_timeout, FLAGS_rpc_threads);
+    ssb.Make_Reg_Object(FLAGS_registry_host, FLAGS_base_service + FLAGS_instance_name, FLAGS_access_host);
+
+    auto server = fsb.Build();
+    server->Start();
+
+    return 0;
+}
