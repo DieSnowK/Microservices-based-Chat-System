8da2f8e2e0042566c22c9d20cb8c9aeec818c505
DieSnowK <1752351098@qq.com>
2024-09-26 07:59:33 +0000

    Adjust file_client test

--- file/test/file_client.cc
+++ file/test/file_client.cc
@@ -19,12 +19,11 @@ DEFINE_string(file_service, "/service/file_service", "Name of the current instan
 SnowK::ServiceChannel::ChannelPtr channel;
 std::string single_file_id;
 
-TEST(put_test, single_file)
+TEST(PUT_TEST, SINGLE_FILE)
 {
-    // 1. 读取当前目录下的指定文件数据
     std::string body;
     ASSERT_TRUE(SnowK::ReadFile("./Makefile", body));
-    // 2. 实例化rpc调用客户端对象，发起rpc调用
+
     SnowK::FileService_Stub stub(channel.get());
 
     SnowK::PutSingleFileReq req;
@@ -37,17 +36,16 @@ TEST(put_test, single_file)
     SnowK::PutSingleFileRsp *rsp = new SnowK::PutSingleFileRsp();
     stub.PutSingleFile(cntl, &req, rsp, nullptr);
     ASSERT_FALSE(cntl->Failed());
-    // 3. 检测返回值中上传是否成功
+
     ASSERT_TRUE(rsp->success());
     ASSERT_EQ(rsp->file_info().file_size(), body.size());
     ASSERT_EQ(rsp->file_info().file_name(), "Makefile");
     single_file_id = rsp->file_info().file_id();
-    LOG_DEBUG("文件ID：{}", rsp->file_info().file_id());
+    LOG_DEBUG("File ID: {}", rsp->file_info().file_id());
 }
 
 TEST(get_test, single_file)
 {
-    // 先发起Rpc调用，进行文件下载
     SnowK::FileService_Stub stub(channel.get());
     SnowK::GetSingleFileReq req;
     SnowK::GetSingleFileRsp *rsp;
@@ -57,9 +55,10 @@ TEST(get_test, single_file)
     brpc::Controller *cntl = new brpc::Controller();
     rsp = new SnowK::GetSingleFileRsp();
     stub.GetSingleFile(cntl, &req, rsp, nullptr);
+
     ASSERT_FALSE(cntl->Failed());
     ASSERT_TRUE(rsp->success());
-    // 将文件数据，存储到文件中
+
     ASSERT_EQ(single_file_id, rsp->file_data().file_id());
     SnowK::WriteFile("make_file_download", rsp->file_data().file_content());
 }
@@ -68,12 +67,11 @@ std::vector<std::string> multi_file_id;
 
 TEST(put_test, multi_file)
 {
-    // 1. 读取当前目录下的指定文件数据
     std::string body1;
     ASSERT_TRUE(SnowK::ReadFile("./base.pb.h", body1));
     std::string body2;
     ASSERT_TRUE(SnowK::ReadFile("./file.pb.h", body2));
-    // 2. 实例化rpc调用客户端对象，发起rpc调用
+
     SnowK::FileService_Stub stub(channel.get());
 
     SnowK::PutMultiFileReq req;
@@ -93,18 +91,17 @@ TEST(put_test, multi_file)
     SnowK::PutMultiFileRsp *rsp = new SnowK::PutMultiFileRsp();
     stub.PutMultiFile(cntl, &req, rsp, nullptr);
     ASSERT_FALSE(cntl->Failed());
-    // 3. 检测返回值中上传是否成功
     ASSERT_TRUE(rsp->success());
+
     for (int i = 0; i < rsp->file_info_size(); i++)
     {
         multi_file_id.push_back(rsp->file_info(i).file_id());
-        LOG_DEBUG("文件ID：{}", multi_file_id[i]);
+        LOG_DEBUG("File ID: {}", multi_file_id[i]);
     }
 }
 
 TEST(get_test, multi_file)
 {
-    // 先发起Rpc调用，进行文件下载
     SnowK::FileService_Stub stub(channel.get());
     SnowK::GetMultiFileReq req;
     SnowK::GetMultiFileRsp *rsp;
@@ -117,9 +114,10 @@ TEST(get_test, multi_file)
     stub.GetMultiFile(cntl, &req, rsp, nullptr);
     ASSERT_FALSE(cntl->Failed());
     ASSERT_TRUE(rsp->success());
-    // 将文件数据，存储到文件中
+
     ASSERT_TRUE(rsp->file_data().find(multi_file_id[0]) != rsp->file_data().end());
     ASSERT_TRUE(rsp->file_data().find(multi_file_id[1]) != rsp->file_data().end());
+    
     auto map = rsp->file_data();
     auto file_data1 = map[multi_file_id[0]];
     SnowK::WriteFile("base_download_file1", file_data1.file_content());
