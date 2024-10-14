649f23156a3c56bced94ef0ec84e80531d968661
DieSnowK <1752351098@qq.com>
2024-10-14 12:10:36 +0000

    friend_apply.hpp

---
+++ odb/friend_apply.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <string>
+#include <cstddef>
+#include <odb/core.hxx>
+
+namespace SnowK
+{
+        #pragma db object table("friend_apply")
+    class FriendApply
+    {
+    public:
+        FriendApply() {}
+        FriendApply(const std::string &eid,
+                    const std::string &uid, const std::string &pid) 
+            : _user_id(uid)
+            , _peer_id(pid)
+            , _event_id(eid)
+        {}
+
+        std::string Event_Id() const 
+        { 
+            return _event_id; 
+        }
+        void Event_Id(std::string &eid) 
+        { 
+            _event_id = eid; 
+        }
+
+        std::string User_Id() const 
+        { 
+            return _user_id; 
+        }
+        void User_Id(std::string &uid) 
+        { 
+            _user_id = uid; 
+        }
+
+        std::string Peer_Id() const 
+        { 
+            return _peer_id; 
+        }
+        void Peer_Id(std::string &uid) 
+        { 
+            _peer_id = uid; 
+        }
+
+    private:
+        friend class odb::access;
+        #pragma db id auto
+        unsigned long _id;
+        #pragma db type("varchar(64)") index unique
+        std::string _event_id;
+        #pragma db type("varchar(64)") index
+        std::string _user_id;
+        #pragma db type("varchar(64)") index
+        std::string _peer_id;
+    };
+}
