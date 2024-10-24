22be28b7d3cdcb074568db6d15a9398d2784f5c9
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-10-24 08:25:37 +0800

    Init Project

---
+++ client/build/Desktop_Qt_6_8_0_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_client_FILE C:/Users/w1752/Desktop/My_Repository/Microservices-based-Chat-System/client/build/Desktop_Qt_6_8_0_MSVC2022_64bit-Debug/client.exe)
+set(__QT_DEPLOY_TARGET_client_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_client_RUNTIME_DLLS E:/Qt/6.8.0/msvc2022_64/bin/Qt6Widgetsd.dll;E:/Qt/6.8.0/msvc2022_64/bin/Qt6Guid.dll;E:/Qt/6.8.0/msvc2022_64/bin/Qt6Cored.dll)
---
+++ client/model/data.hpp
@@ -0,0 +1,51 @@
+#include <QString>
+#include <QIcon>
+
+namespace model
+{
+    class UserInfo
+    {
+        QString userId = "";
+        QString nickname = "";
+        QString description = "";
+        QString phone = "";
+        QIcon avatar;
+    };
+
+    enum class MessageType
+    {
+        TEXT_TYPE,
+        IMAGE_TYPE,
+        FILE_TYPE,
+        SPECCH_TYPE
+    };
+
+    class Message
+    {
+    public:
+        QString messageId = "";
+        QString chatSessionId = "";
+        QString time = "";
+        MessageType msgType = MessageType::TEXT_TYPE;
+        UserInfo sender;
+        QByteArray content;
+        QString fileId = "";
+        QString fileName = "";
+
+        static Message MakeMessage(MessageType msgType, const QString& chatSessionId, const UserInfo& sender,
+                                   const QByteArray& content, const QString& extraInfo)
+        {
+            switch(msgType)
+            {
+            case MessageType::TEXT_TYPE:
+                break;
+            case MessageType::IMAGE_TYPE:
+                break;
+            case MessageType::FILE_TYPE:
+                break;
+            case MessageType::SPECCH_TYPE:
+                break;
+            }
+        }
+    };
+}
