590fdc6e9090373eec37e14519607dda458be375
DieSnowK <1752351098@qq.com>
2024-10-24 02:20:27 +0000

    core class todo

--- client/model/data.hpp
+++ client/model/data.hpp
@@ -1,9 +1,75 @@
+#pragma once
+
 #include <QString>
 #include <QIcon>
 #include <QUuid>
+#include <QDateTime>
+#include <QFile>
+#include <QFileInfo>
+#include <QDebug>
 
 namespace model
 {
+    namespace Util
+    {
+        static inline QString GetFileName(const QString& path)
+        {
+            return QFileInfo(path).fileName();
+        }
+
+        #define TAG QString("[%1:%2]").arg(model::Util::GetFileName(__FILE__), QString::number(__LINE__))
+        #define LOG() qDebug().noquote() << TAG
+
+        static inline QString FormatTime(int64_t timestamp)
+        {
+            // TODO Test
+            return QDateTime(QDateTime::fromSecsSinceEpoch(timestamp)).toString("MM-dd HH:mm:ss");
+        }
+
+        static inline int64_t GetTime()
+        {
+            return QDateTime::currentSecsSinceEpoch();
+        }
+
+        static inline QIcon MakeIcon(const QByteArray& byteArray)
+        {
+            QPixmap pixmap;
+            pixmap.loadFromData(byteArray);
+
+            return QIcon(pixmap);
+        }
+
+        static inline QByteArray LoadFileToByteArray(const QString& path)
+        {
+            QFile file(path);
+            if(!file.open(QFile::ReadOnly))
+            {
+                LOG() << "Failed to open file";
+                return QByteArray();
+            }
+
+            QByteArray content = file.readAll();
+
+            file.close();
+            return content;
+        }
+
+        static inline void WriteByteArrayToFile(const QString& path, const QByteArray& content)
+        {
+            QFile file(path);
+            if(!file.open(QFile::WriteOnly))
+            {
+                LOG() << "Failed to open file";
+                return;
+            }
+
+            file.write(content);
+            file.flush();
+
+            file.close();
+        }
+    } // end of namespace Util
+
     class UserInfo
     {
         QString userId = "";
@@ -33,6 +99,7 @@ namespace model
         QString fileId = "";
         QString fileName = "";
 
+        // Factory Pattern
         static Message MakeMessage(MessageType msgType, const QString& chatSessionId, const UserInfo& sender,
                                    const QByteArray& content, const QString& extraInfo)
         {
