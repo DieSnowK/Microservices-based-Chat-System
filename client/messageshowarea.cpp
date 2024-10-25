750742b7df000c89b368d11ce62aee9dc83cb6db
DieSnowK <1752351098@qq.com>
2024-10-25 07:12:18 +0000

    Message TODO

--- client/messageshowarea.cpp
+++ client/messageshowarea.cpp
@@ -40,3 +40,74 @@ MessageShowArea::MessageShowArea()
     }
 #endif
 }
+
+MessageItem::MessageItem(bool isLeft) 
+    : isLeft(isLeft)
+{}
+
+MessageItem *MessageItem::MakeMessageItem(bool isLeft, const Message &message)
+{
+    MessageItem* messageItem = new MessageItem(isLeft);
+
+    QGridLayout* layout = new QGridLayout();
+    layout->setContentsMargins(30, 10, 40, 0);
+    layout->setSpacing(10);
+    messageItem->setMinimumHeight(100);
+    messageItem->setLayout(layout);
+
+    QPushButton* avatarBtn = new QPushButton();
+    avatarBtn->setFixedSize(40, 40);
+    avatarBtn->setIconSize(QSize(40, 40));
+    avatarBtn->setIcon(message.sender.avatar);
+    avatarBtn->setStyleSheet("QPushButton { border: none; }");
+    if (isLeft)
+    {
+        layout->addWidget(avatarBtn, 0, 0, 2, 1, Qt::AlignTop | Qt::AlignLeft);
+    }
+    else
+    {
+        layout->addWidget(avatarBtn, 0, 1, 2, 1, Qt::AlignTop | Qt::AlignLeft);
+    }
+
+    QLabel* nameLabel = new QLabel();
+    nameLabel->setText(message.sender.nickname + " | " + message.time);
+    nameLabel->setAlignment(Qt::AlignBottom);
+    nameLabel->setStyleSheet("QLabel { font-size: 12px; color: rgb(178, 178, 178); }");
+    if (isLeft)
+    {
+        layout->addWidget(nameLabel, 0, 1);
+    }
+    else
+    {
+        layout->addWidget(nameLabel, 0, 0, Qt::AlignRight);
+    }
+
+    QWidget* contentWidget = nullptr;
+    switch (message.messageType)
+    {
+    case TEXT_TYPE:
+        contentWidget = MakeTextMessageItem(isLeft, message.content);
+        break;
+    case IMAGE_TYPE:
+        contentWidget = MakeImageMessageItem(isLeft, message.fileId, message.content);
+        break;
+    case FILE_TYPE:
+        contentWidget = MakeFileMessageItem(isLeft, message);
+        break;
+    case SPEECH_TYPE:
+        contentWidget = MakeSpeechMessageItem(isLeft, message);
+        break;
+    default:
+        LOG() << "Error MessageType, messageType = " << (int)message.messageType;
+    }
+    if (isLeft)
+    {
+        layout->addWidget(contentWidget, 1, 1);
+    }
+    else
+    {
+        layout->addWidget(contentWidget, 1, 0);
+    }
+
+    return messageItem;
+}
--- client/messageshowarea.h
+++ client/messageshowarea.h
@@ -5,15 +5,41 @@
 #include <QWidget>
 #include <QScrollBar>
 #include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+
+// using model::Message;
 
 class MessageShowArea : public QScrollArea
 {
     Q_OBJECT
 public:
     MessageShowArea();
+    
+    void AddMessage(bool isLeft, const Message& message);
+    void AddFrontMessage(bool isLeft, const Message& message);
 
 private:
     QWidget *container;
+};
+
+class MessageItem : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MessageItem(bool isLeft);
+
+    // Factory Pattern
+    static MessageItem* MakeMessageItem(bool isLeft, const Message& message);
+    
+    static QWidget* MakeTextMessageItem(bool isLeft, const QString& text);
+    static QWidget* MakeImageMessageItem(bool isLeft, const QString& fileId, const QByteArray& content);
+    static QWidget* MakeFileMessageItem(bool isLeft, const Message& message);
+    static QWidget* MakeSpeechMessageItem(bool isLeft, const Message& message);
+
+private:
+    bool isLeft;
 };
 
 #endif // MESSAGESHOWAREA_H
