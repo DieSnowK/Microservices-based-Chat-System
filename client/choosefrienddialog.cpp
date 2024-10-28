f7c16216fac0a41af49f3bc8247f5e1525e2eef1
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-10-28 12:07:10 +0800

    TODO

--- client/choosefrienddialog.cpp
+++ client/choosefrienddialog.cpp
@@ -1,4 +1,101 @@
 #include "choosefrienddialog.h"
+#include "debug.hpp"
+
+////////////////////////////////////////////////
+/// ChooseFriendItem
+////////////////////////////////////////////////
+
+ChooseFriendItem::ChooseFriendItem(ChooseFriendDialog *owner, const QString &userId,
+                                   const QIcon &avatar, const QString &name, bool checked)
+    : userId(userId)
+{
+    this->setFixedHeight(50);
+    this->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+
+    QHBoxLayout* layout = new QHBoxLayout();
+    layout->setSpacing(10);
+    layout->setContentsMargins(20, 0, 20, 0);
+    this->setLayout(layout);
+
+    checkBox = new QCheckBox();
+    checkBox->setChecked(checked);
+    checkBox->setFixedSize(25, 25);
+    QString style = "QCheckBox { background-color: transparent; }"
+                    "QCheckBox::indicator { width: 20px; height: 20px;"
+                    "image: url(:/resource/image/unchecked.png); }";
+    style += "QCheckBox::indicator:checked { image: url(:/resource/image/checked.png);}";
+    checkBox->setStyleSheet(style);
+
+    avatarBtn = new QPushButton();
+    avatarBtn->setFixedSize(40, 40);
+    avatarBtn->setIconSize(QSize(40, 40));
+    avatarBtn->setIcon(avatar);
+
+    nameLabel = new QLabel();
+    nameLabel->setText(name);
+    nameLabel->setStyleSheet("QLabel {background-color: transparent;}");
+
+    layout->addWidget(checkBox);
+    layout->addWidget(avatarBtn);
+    layout->addWidget(nameLabel);
+
+    connect(checkBox, &QCheckBox::toggled, this, [=](bool checked)
+    {
+        if (checked)
+        {
+            owner->AddSelectedFriend(userId, avatar, name);
+        }
+        else
+        {
+            owner->DeleteSelectedFriend(userId);
+        }
+    });
+}
+
+void ChooseFriendItem::paintEvent(QPaintEvent *event)
+{
+    (void) event;
+
+    QPainter painter(this);
+    if (isHover)
+    {
+        painter.fillRect(this->rect(), QColor(230, 230, 230));
+    }
+    else
+    {
+        painter.fillRect(this->rect(), QColor(255, 255, 255));
+    }
+}
+
+void ChooseFriendItem::enterEvent(QEnterEvent *event)
+{
+    (void) event;
+    isHover = true;
+
+    // "update()" is equivalent to "update interface"
+    this->update(); // ==> this->repaint();
+}
+
+void ChooseFriendItem::leaveEvent(QEvent *event)
+{
+    (void) event;
+    isHover =  false;
+    this->update();
+}
+
+const QString &ChooseFriendItem::GetUserId() const
+{
+    return userId;
+}
+
+QCheckBox *ChooseFriendItem::GetCheckBox()
+{
+    return checkBox;
+}
+
+////////////////////////////////////////////////
+/// ChooseFriendDialog
+////////////////////////////////////////////////
 
 ChooseFriendDialog::ChooseFriendDialog(QWidget *parent)
 {
@@ -97,7 +194,7 @@ void ChooseFriendDialog::InitRight(QHBoxLayout *layout)
     QPushButton* cancelBtn = new QPushButton();
     cancelBtn->setFixedHeight(40);
     cancelBtn->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
-    cancelBtn->setText("Cancle");
+    cancelBtn->setText("Cancel");
     cancelBtn->setStyleSheet(style);
 
     gridLayout->addWidget(tipLabel, 0, 0, 1, 9);
@@ -105,7 +202,7 @@ void ChooseFriendDialog::InitRight(QHBoxLayout *layout)
     gridLayout->addWidget(okBtn, 2, 1, 1, 3);
     gridLayout->addWidget(cancelBtn, 2, 5, 1, 3);
 
-#if TEST_UI
+#if 0
     QIcon defaultAvatar(":/resource/image/defaultAvatar.png");
     for (int i = 0; i < 10; ++i)
     {
@@ -114,7 +211,65 @@ void ChooseFriendDialog::InitRight(QHBoxLayout *layout)
 #endif
 
     // connect(okBtn, &QPushButton::clicked, this, &ChooseFriendDialog::clickOkBtn);
-    // connect(cancelBtn, &QPushButton::clicked, this, [=]() {
+    // connect(cancelBtn, &QPushButton::clicked, this, [=]()
+    // {
     //     this->close();
     // });
 }
+
+void ChooseFriendDialog::AddFriend(const QString &userId, const QIcon &avatar, const QString &name, bool checked)
+{
+    ChooseFriendItem* item = new ChooseFriendItem(this, userId, avatar, name, checked);
+    totalContainer->layout()->addWidget(item);
+}
+
+void ChooseFriendDialog::AddSelectedFriend(const QString &userId, const QIcon &avatar, const QString &name)
+{
+    ChooseFriendItem* item = new ChooseFriendItem(this, userId, avatar, name, true);
+    selectedContainer->layout()->addWidget(item);
+}
+
+void ChooseFriendDialog::DeleteSelectedFriend(const QString &userId)
+{
+    // // 遍历 selectedContainer 中的每个 Item, 对比每个 Item 里的 userId , 是否是要删除的 userId.
+    // QVBoxLayout* vlayout = dynamic_cast<QVBoxLayout*>(selectedContainer->layout());
+    // // 由于是要 "遍历" + "删除" 需要从后往前进行
+    // for (int i = vlayout->count() - 1; i >= 0; --i)
+    // {
+    //     auto* item = vlayout->itemAt(i);
+    //     if (item == nullptr || item->widget() == nullptr)
+    //     {
+    //         continue;
+    //     }
+    //     ChooseFriendItem* chooseFriendItem = dynamic_cast<ChooseFriendItem*>(item->widget());
+    //     // 判定当前的 Item 的 userId 是否是要删除的 userId
+    //     if (chooseFriendItem->GetUserId() != userId)
+    //     {
+    //         continue;
+    //     }
+    //     vlayout->removeWidget(chooseFriendItem);
+    //     // 此处直接使用 delete 可能导致程序直接崩溃. 因为 delete 该对象的时候, 该对象内部的 QCheckBox 还在使用中 (触发着信号槽呢)
+    //     // 改成 deleteLater, 就相当于把 delete 操作委托给 Qt 自身来完成了. 告诉 Qt 框架说, 你要删除这个对象. 至于啥时候删除 Qt
+    //     // 会确保在 Qt 自身用完了之后, 去真正删除.
+    //     // delete chooseFriendItem;
+    //     chooseFriendItem->deleteLater();
+    // }
+
+    // // 再遍历一下左侧列表, 把左侧列表中对应 item 的 checkBox 勾选状态取消掉.
+    // QVBoxLayout* vlayoutLeft = dynamic_cast<QVBoxLayout*>(totalContainer->layout());
+    // for (int i = 0; i < vlayoutLeft->count(); ++i) {
+    //     auto* item = vlayoutLeft->itemAt(i);
+    //     if (item == nullptr || item->widget() == nullptr)
+    //     {
+    //         continue;
+    //     }
+    //     ChooseFriendItem* chooseFriendItem = dynamic_cast<ChooseFriendItem*>(item->widget());
+    //     if (chooseFriendItem->GetUserId() != userId)
+    //     {
+    //         continue;
+    //     }
+    //     // 取消 checkBox 选中状态
+    //     chooseFriendItem->GetCheckBox()->setChecked(false);
+    // }
+}
+
--- client/choosefrienddialog.h
+++ client/choosefrienddialog.h
@@ -8,6 +8,44 @@
 #include <QScrollBar>
 #include <QLabel>
 #include <QPushButton>
+#include <QCheckBox>
+#include <QPainter>
+
+class ChooseFriendDialog;
+
+////////////////////////////////////////////////
+/// ChooseFriendItem
+////////////////////////////////////////////////
+
+class ChooseFriendItem : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ChooseFriendItem(ChooseFriendDialog* owner, const QString& userId,
+                     const QIcon& avatar, const QString& name, bool checked);
+
+    void paintEvent(QPaintEvent* event) override;
+    void enterEvent(QEnterEvent* event) override;
+    void leaveEvent(QEvent* event) override;
+
+    const QString& GetUserId() const;
+    QCheckBox* GetCheckBox();
+
+private:
+    bool isHover = false;
+
+    QCheckBox* checkBox;
+    QPushButton* avatarBtn;
+    QLabel* nameLabel;
+    ChooseFriendDialog* owner;
+
+    QString userId;
+};
+
+////////////////////////////////////////////////
+/// ChooseFriendDialog
+////////////////////////////////////////////////
 
 class ChooseFriendDialog : public QDialog
 {
@@ -18,6 +56,10 @@ public:
 
     void InitLeft(QHBoxLayout* layout);
     void InitRight(QHBoxLayout* layout);
+
+    void AddFriend(const QString& userId, const QIcon& avatar, const QString& name, bool checked);
+    void AddSelectedFriend(const QString& userId, const QIcon& avatar, const QString& name);
+    void DeleteSelectedFriend(const QString& userId);
 
 private:
     QWidget* totalContainer;
