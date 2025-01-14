d3273e798cfd9338864acd8b36de512ecfd5e305
DieSnowK <1752351098@qq.com>
2025-01-14 17:44:36 +0000

    Server Configuration

---
+++ server/build/message/CMakeFiles/message_client.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/snowk/repository/Microservices-based-Chat-System/server/build/message/base.pb.cc" "message/CMakeFiles/message_client.dir/base.pb.cc.o" "gcc" "message/CMakeFiles/message_client.dir/base.pb.cc.o.d"
+  "/home/snowk/repository/Microservices-based-Chat-System/server/build/message/file.pb.cc" "message/CMakeFiles/message_client.dir/file.pb.cc.o" "gcc" "message/CMakeFiles/message_client.dir/file.pb.cc.o.d"
+  "/home/snowk/repository/Microservices-based-Chat-System/server/build/message/message.pb.cc" "message/CMakeFiles/message_client.dir/message.pb.cc.o" "gcc" "message/CMakeFiles/message_client.dir/message.pb.cc.o.d"
+  "/home/snowk/repository/Microservices-based-Chat-System/server/message/test/test/message.client.cc" "message/CMakeFiles/message_client.dir/test/test/message.client.cc.o" "gcc" "message/CMakeFiles/message_client.dir/test/test/message.client.cc.o.d"
+  "/home/snowk/repository/Microservices-based-Chat-System/server/build/message/user.pb.cc" "message/CMakeFiles/message_client.dir/user.pb.cc.o" "gcc" "message/CMakeFiles/message_client.dir/user.pb.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
