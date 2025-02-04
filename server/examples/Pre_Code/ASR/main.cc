c1ecca9dc92490a8476de40630aaa2f7528fc842
DieSnowK <1752351098@qq.com>
2025-02-04 09:23:08 +0000

    Server ASR Test Done

--- server/examples/Pre_Code/ASR/main.cc
+++ server/examples/Pre_Code/ASR/main.cc
@@ -2,9 +2,9 @@
 
 int main()
 {
-    std::string app_id = "";
-    std::string api_key = "";
-    std::string secret_key = "";
+    std::string app_id = "115680523";
+    std::string api_key = "NZ5oucBN0wWQ6uXZLt7c4TXx";
+    std::string secret_key = "8YcwL4DegU2Um0D6WNPO4CBcaLgiDgnQ";
 
     SnowK::ASRClient client(app_id, api_key, secret_key);
 
